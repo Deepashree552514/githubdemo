c14cae50691d58f65c5ca597138f623af1743e73
unknown <deepashree.jnnce@gmail.com>
2022-11-26 16:57:53 +0530

    mimorchange

--- src/main/java/com/javatechie/githincdcdactions/GithinCdcdActionsApplication.java
+++ src/main/java/com/javatechie/githincdcdactions/GithinCdcdActionsApplication.java
@@ -2,7 +2,7 @@ package com.javatechie.githincdcdactions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.find.annotation.RestController;
 @SpringBootApplication
 @RestController
 
